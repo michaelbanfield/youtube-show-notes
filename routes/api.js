ca1ffe7c738f3ab308b373edacb007df63393da7
Michae <shrapnelbikes@gmail.com>
2013-07-03 14:15:52 +1000

    adding back in redis dependancy

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,8 @@ if (process.env.REDISTOGO_URL) {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
     var client = require("redis").createClient(rtg.port, rtg.hostname);
 
+    client.auth(rtg.auth.split(":")[1]);
+
 } else {
     var redis = require("redis"),
         client = redis.createClient();
